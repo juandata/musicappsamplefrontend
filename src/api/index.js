af1ed8e1b10299452ce05aba6810990f485b7bd7
juanbairesdev <juan.tabares@bairesdev.com>
2023-08-04 16:40:42 -0500

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const client = axios.create({
+	// eslint-disable-next-line no-undef
+	baseURL: process.env.REACT_APP_NODE_ENV === "development" ? "http://localhost:3000/api" : "otherurl",
+});
