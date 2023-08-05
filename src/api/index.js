e91c7eddab1cf98655d6ae6a04761e9d3fdeddc1
juanbairesdev <juan.tabares@bairesdev.com>
2023-08-04 19:37:49 -0500

    deleted process as there is an error

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-
+const environment = "development";
 export const client = axios.create({
-	// eslint-disable-next-line no-undef
-	baseURL: process.env.REACT_APP_NODE_ENV === "development" ? "http://localhost:3000/api" : "https://musicappsamplebackend.onrender.com/api",
+  // eslint-disable-next-line no-undef
+  baseURL:
+    environment === "development"
+      ? "http://localhost:3000/api"
+      : "https://musicappsamplebackend.onrender.com/api",
 });
