650d812edb41c773c97783242e5a4928fc8b0acc
juanbairesdev <juan.tabares@bairesdev.com>
2023-08-04 19:28:02 -0500

    added url

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const client = axios.create({
 	// eslint-disable-next-line no-undef
-	baseURL: process.env.REACT_APP_NODE_ENV === "development" ? "http://localhost:3000/api" : "otherurl",
+	baseURL: process.env.REACT_APP_NODE_ENV === "development" ? "http://localhost:3000/api" : "https://musicappsamplebackend.onrender.com/api",
 });
